f094f3013c847922d3e09db290da481f4807a0e3
Jonatan Lemes <jonatan@jslsolucoes.com>
2021-03-06 15:46:24 -0300

    update docker image

--- metrosp-tic-docker/create-image.sh
+++ metrosp-tic-docker/create-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Github username: " username
 read -s -p "Github password: " password
-docker build --rm --build-arg GIT_USERNAME=$username --build-arg GIT_PASSWORD=$password -t jslsolucoes/metrosp-tic-bot:latest .
+docker build --rm --build-arg BOT_URL=http://localhost:8080 --build-arg GIT_USERNAME=$username --build-arg GIT_PASSWORD=$password -t jslsolucoes/metrosp-tic-bot:latest .
