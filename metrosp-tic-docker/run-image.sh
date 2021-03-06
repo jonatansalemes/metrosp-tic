5bb8999cefb3a26f67445d0add7f2d4844a49b4e
Jonatan Lemes <jonatan@jslsolucoes.com>
2021-03-06 18:20:01 -0300

    update flows

--- metrosp-tic-docker/run-image.sh
+++ metrosp-tic-docker/run-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p5000:5000 -p5001:5001 -p8080:8080 jslsolucoes/metrosp-tic-bot:latest
+docker run --rm -p5000:5000 -p5001:5001 -p8080:8080 -p8081:8081 jslsolucoes/metrosp-tic-bot:latest
