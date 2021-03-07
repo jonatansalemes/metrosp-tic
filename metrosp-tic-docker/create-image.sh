7b9ba96a388a4c6a47cbb07cd3795b21fea061c2
Jonatan Lemes <jonatan@jslsolucoes.com>
2021-03-07 02:03:32 -0300

    update flows

--- metrosp-tic-docker/create-image.sh
+++ metrosp-tic-docker/create-image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-read -p "Bot url base:\n" bot_url
-read -p "Github username:\n" username
-read -s -p "Github password:\n" password
+echo "Bot url base:"
+read bot_url
+echo "Github username:"
+read username
+echo "Github password:"
+read -s password
 docker build --rm --build-arg BOT_URL=$bot_url --build-arg GIT_USERNAME=$username --build-arg GIT_PASSWORD=$password -t jslsolucoes/metrosp-tic-bot:latest .
